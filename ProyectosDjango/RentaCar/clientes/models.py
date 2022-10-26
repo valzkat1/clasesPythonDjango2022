ae000ecdd07f23e126a816d6b2c8ddc86bcb8bc5
valzkat1 <valzkat12@gmail.com>
2022-10-26 07:59:18 -0500

    Clase Oct 26

---
+++ ProyectosDjango/RentaCar/clientes/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+class Clientes(models.Model):
+    nombre=models.CharField(max_length=50)
+    edad=models.IntegerField()
+    email=models.EmailField()
+    fechaNacimi=models.DateField()
+    
+
+class Tipos(models.Model):
+    nombre=models.CharField(max_length=20)
+    descripcion=models.CharField(max_length=100)
+
