ccdf8878105242fd4a0f76c6b755d3ab7b7aa6b9
valzkat1 <valzkat12@gmail.com>
2022-11-01 07:12:08 -0500

    Clase Nov 1 Parte 1

--- ProyectosDjango/RentaCar/clientes/admin.py
+++ ProyectosDjango/RentaCar/clientes/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from clientes.models import Clientes
+from clientes.models import Venta
+from clientes.models import Tipos
 
-# Register your models here.
+admin.site.register(Clientes)
+admin.site.register(Venta)
+admin.site.register(Tipos)
--- ProyectosDjango/RentaCar/clientes/models.py
+++ ProyectosDjango/RentaCar/clientes/models.py
@@ -4,14 +4,25 @@ from django.db import models
 class Clientes(models.Model):
     nombre=models.CharField(max_length=50)
     edad=models.IntegerField()
-    email=models.EmailField()
+    email=models.EmailField(blank=True,null=True)
     fechaNacimi=models.DateField()
+    def __str__(self):
+        return 'Nombre: %s, Edad: %s, Email: %s, FechaN: %s'%(self.nombre,self.edad,self.email,self.fechaNacimi)
+    
     
 
 class Tipos(models.Model):
     nombre=models.CharField(max_length=20)
     descripcion=models.CharField(max_length=100)
-    
-    
+    def __str__(self):
+        return 'Tipo: %s, Descripcion: %s '%(self.nombre,self.descripcion)
+   
+class Venta(models.Model):
+    fecha=models.DateField()
+    total=models.IntegerField()
+    cliente=models.CharField(max_length=100)
+    def __str__(self):
+        return 'Fecha: %s, Total: %s, Cliente: %s '%(self.fecha,self.total,self.cliente)
+        
  ## C R U D   
 
