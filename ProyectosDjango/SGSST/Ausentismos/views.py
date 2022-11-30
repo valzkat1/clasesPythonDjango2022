7bf7a7cc59b4f6e7bc303f78bb446f78892c87fb
valzkat1 <valzkat12@gmail.com>
2022-11-30 08:00:26 -0500

    Clase Nov 30

--- ProyectosDjango/SGSST/Ausentismos/views.py
+++ ProyectosDjango/SGSST/Ausentismos/views.py
@@ -11,7 +11,65 @@ def crearAusentismo(request):
        # MiFormulario.fields['totalIncapacidad']='0'
         
         if MiFormulario.is_valid():
-             MiFormulario.save()
+             Objincapacidad = MiFormulario.save(commit=False)
+             datosForm = MiFormulario.cleaned_data
+             tipoIncapaci = datosForm.get('tipoIncapacidad')
+             totalDias    = datosForm.get('totalDias')
+             salarioDia   = float(datosForm.get('salarioDia'))
+             if tipoIncapaci=='Enfermedad Comun':
+                 if totalDias<3:
+                   Objincapacidad.totalEmpresa= str(totalDias*salarioDia)
+                   Objincapacidad.totalArl='0'
+                   Objincapacidad.totalEPS='0'
+                   Objincapacidad.totalAFP='0'
+                   Objincapacidad.totalIncapacidad=str(totalDias*salarioDia)
+                 elif totalDias < 181:
+                   Objincapacidad.totalEmpresa= str(2*salarioDia)
+                   Objincapacidad.totalArl='0'
+                   Objincapacidad.totalEPS= str((totalDias-2)*salarioDia*0.6667)
+                   Objincapacidad.totalAFP='0'
+                   Objincapacidad.totalIncapacidad=str(float(Objincapacidad.totalEmpresa)+float(Objincapacidad.totalEPS))
+                 elif totalDias < 541:
+                   Objincapacidad.totalEmpresa= str(2*salarioDia)
+                   Objincapacidad.totalArl='0'
+                   Objincapacidad.totalEPS= str((178)*salarioDia*0.6667)
+                   Objincapacidad.totalAFP= str((totalDias-180)*salarioDia*0.5)
+                   Objincapacidad.totalIncapacidad=str(float(Objincapacidad.totalEmpresa)+float(Objincapacidad.totalEPS)+float(Objincapacidad.totalAFP))
+                 else:
+                   # temporalmente se implementa igual al intervalo anterior  
+                   Objincapacidad.totalEmpresa= str(2*salarioDia)
+                   Objincapacidad.totalArl='0'
+                   Objincapacidad.totalEPS= str((178)*salarioDia*0.6667)
+                   Objincapacidad.totalAFP= str((totalDias-180)*salarioDia*0.5)
+                   Objincapacidad.totalIncapacidad=str(float(Objincapacidad.totalEmpresa)+float(Objincapacidad.totalEPS)+float(Objincapacidad.totalAFP))
+                         
+             elif tipoIncapaci == 'Licencia Mat-Pat': 
+                   Objincapacidad.totalEmpresa= '0'
+                   Objincapacidad.totalArl='0'
+                   Objincapacidad.totalEPS= str((120)*salarioDia)
+                   Objincapacidad.totalAFP= '0'
+                   Objincapacidad.totalIncapacidad=str(float(Objincapacidad.totalEPS))
+             elif (tipoIncapaci == 'Accidente Laboral'): 
+                   Objincapacidad.totalEmpresa= '0'
+                   Objincapacidad.totalArl= str((totalDias)*salarioDia)
+                   Objincapacidad.totalEPS= '0'
+                   Objincapacidad.totalAFP= '0'
+                   Objincapacidad.totalIncapacidad=str(float(Objincapacidad.totalArl))
+             elif ( tipoIncapaci == 'Enfermedad Laboral'):  
+                   Objincapacidad.totalEmpresa= '0'
+                   Objincapacidad.totalArl= str((totalDias)*salarioDia)
+                   Objincapacidad.totalEPS= '0'
+                   Objincapacidad.totalAFP= '0'
+                   Objincapacidad.totalIncapacidad=str(float(Objincapacidad.totalArl))
+             elif tipoIncapaci == 'Accidente de Transito': 
+                   Objincapacidad.totalEmpresa= str(2*salarioDia)
+                   Objincapacidad.totalArl= str((totalDias-2)*salarioDia*0.67)
+                   Objincapacidad.totalEPS= '0'
+                   Objincapacidad.totalAFP= '0'
+                   Objincapacidad.totalIncapacidad=str(float(Objincapacidad.totalArl)+float(Objincapacidad.totalEmpresa))          
+                       
+          
+             Objincapacidad.save()
              return redirect("listarInca")
         else:
              return render(request,"Ausentismos/formularios.html",{"tituloFormulario":"Registrar Incapacidad","actionFormulario":"/ausentismos/crear/","contenidoFormulario":MiFormulario})
